d2011815a0a2599b998f3b2817198f3ce22ce893
Ben <ben9910@gmail.com>
2014-02-25 18:21:20 +0000

    Init commit, db created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clonemarks::Application.config.secret_token = 'ce0ec18e8df68e150d367461ac59363a0e3dd6357e59136b15fe67cf3572896044c91a1e7553e06bb9a45c266b16a1cb38b4584b85dd3c14954ffe94ae9dc683'
