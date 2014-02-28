0980b158b9477080417d6af3c4c7798ba90fa91a
Ben <ben9910@gmail.com>
2014-02-28 01:34:20 +0000

    Created bookmarks controller

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,34 @@
+class BookmarksController < ApplicationController
+	before_filter :set_bookmark, except: [:index, :new, :create]
+
+	def new
+		@bookmark = Bookmark.new
+	end
+
+	def create
+		@bookmark = Bookmark.new(params[:bookmark])
+		@bookmark.user = current.user
+		if @bookmark.save
+			flash[:notice] = "Bookmark saved"
+			redirect_to @bookmark
+		else
+			flash[:error] => "Bookmark not saved"
+			# Redirect?  If so, to where?
+	end
+
+	def destroy
+	end
+
+	def show
+	end
+
+	def index
+		@bookmarks = Bookmark.all
+	end
+
+	private
+
+	def set_bookmark
+		@bookmark = Bookmark.find(params[:id])
+	end
+end
