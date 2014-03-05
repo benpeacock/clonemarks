e1413d41e4847bdbd6010a1b9c1a6b1be3dccb08
Ben <ben9910@gmail.com>
2014-03-05 21:39:33 +0000

    Adding welcome comtroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  rescue_from Cancan::AccessDenied do |exception|
-  	redirect_to root_url, :alert => exeption.message
-  end
+  # rescue_from Cancan::AccessDenied do |exception|
+  # 	redirect_to root_url, :alert => exeption.message
+  # end
 end
