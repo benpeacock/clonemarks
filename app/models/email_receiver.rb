13d88d77559bda51a00a265daeddeb3617c199b0
Ben <ben9910@gmail.com>
2014-03-07 04:11:21 +0000

    2

--- app/models/email_receiver.rb
+++ app/models/email_receiver.rb
@@ -1,7 +1,5 @@
-class EmailReceiver < ActiveRecord::Base
-  # attr_accessible :title, :body
-
-  #setup :api_key => ENV['MAILGUN_API_KEY']
+class EmailReceiver < Incoming::Strategies::Mailgun
+  setup :api_key => ENV['MAILGUN_API_KEY']
 
   def receive(mail)
     bookmark = Bookmark.new_from_email(mail)
