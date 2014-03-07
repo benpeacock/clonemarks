d9a1cc333f87f8ae1d66fd30783684006bb5bcea
Ben <benjaminwpeacock@gmail.com>
2014-03-07 12:20:11 -0700

    9;

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -7,9 +7,9 @@ class Bookmark < ActiveRecord::Base
   def self.new_from_email(mail)
   	bookmark = Bookmark.new
   	Rails.logger.info ">>>> #{mail}.inspect"
-  	bookmark = {:name => "test name", :url => "www.test.com"}
-  	#bookmark[:name] = "test name"
-  	#bookmark[:url] = "test url"
+  	bookmark[:name] = "test name"
+  	bookmark[:topic] = "--- - quidem"
+  	bookmark[:url] = "test url"
   	bookmark
   end
 end
