0a916b5ad348dbf924ec64e23032576ff2443013
Ben <benjaminwpeacock@gmail.com>
2014-03-12 16:20:13 -0600

    Incoming e-mails being fully parsed for user, subject and body

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -7,22 +7,26 @@ class Bookmark < ActiveRecord::Base
   scope :desc, order("bookmarks.updated_at DESC")
 
   def self.new_from_email(mail)
-  	bookmark = Bookmark.new
+  	bookmark = nil
   	# get the user from the e-mail address
     Rails.logger.info ">>> email address is: #{mail.from.first}"
-    bookmark.user = User.find_by_email(mail.from.first)
-    Rails.logger.info ">>>> Bookmark user: #{bookmark.user.inspect}"
-    # strip the tag off the subject and set it as subject
-    bookmark.topic = Topic.where(name: mail.subject.gsub(/#/, '')).first
-    bookmark.topic ||= Topic.create(name: mail.subject.gsub(/#/, ''))
+    user = User.find_by_email(mail.from.first)
+    if user 
+      bookmark = Bookmark.new
+      bookmark.user = user
+      Rails.logger.info ">>>> Bookmark user: #{bookmark.user.inspect}"
+      # strip the tag off the subject and set it as subject
+      bookmark.topic = Topic.where(name: mail.subject.gsub(/#/, '')).first
+      bookmark.topic ||= Topic.create(name: mail.subject.gsub(/#/, ''))
 
-    # strip the hash tag off.
-  	# look up the topic in the db
-  	# get the id of the topic
-  	# save the bookmark using topic id
-  	# bookmark[:topic] = topic ##This just saves the topic, not the id ##
-  	bookmark.url = mail.body.decoded
-    Rails.logger.info ">>>> Bookmark: #{bookmark.inspect}"
+      # strip the hash tag off.
+    	# look up the topic in the db
+    	# get the id of the topic
+    	# save the bookmark using topic id
+    	# bookmark[:topic] = topic ##This just saves the topic, not the id ##
+    	bookmark.url = mail.body.decoded
+      Rails.logger.info ">>>> Bookmark: #{bookmark.inspect}"
+    end
   	bookmark
   end
 end
--- app/models/email_receiver.rb
+++ app/models/email_receiver.rb
@@ -4,6 +4,6 @@ class EmailReceiver < Incoming::Strategies::Mailgun
   def receive(mail)
     bookmark = Bookmark.new_from_email(mail)
     Rails.logger.info ">>> Bookmark is still: #{bookmark.inspect}"
-    bookmark.save
+    bookmark.save if bookmark
   end
 end
